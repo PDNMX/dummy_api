c2f81f353733b4d85e55d368b4e31b9b7c36a72c
Mario Torres <mario291187@gmail.com>
2018-10-24 16:47:12 -0500

    MongoDB

--- routes/s1.js
+++ routes/s1.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+// Connection URL
+const url = 'mongodb://localhost:27017';
+
+// Database Name
+const dbName = 'myproject';
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
     res.json({
