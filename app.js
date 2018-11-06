d96c57cc41eb697ece035da3ed73376f4e0c86e8
Benito Perdomo <benito.perdomo.pdn@gmail.com>
2018-11-06 12:46:56 -0600

    se agrega las rutas para registro de declaraciones

--- app.js
+++ app.js
@@ -6,6 +6,21 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var s1Router = require('./routes/s1');
 
+
+// inicio declaraciones
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var ciudadesRouter = require('./routes/ciudades');
+var entidadesRouter = require('./routes/entidades');
+var estadoscivilesRouter = require('./routes/estadosciviles');
+var regimenmatrimonialRouter = require('./routes/regimenmatrimonial');
+var municipiosRouter = require('./routes/municipios');
+var localidadesRouter = require('./routes/localidades');
+var tipovialidadRouter = require('./routes/tipovialidad');
+var declaracionesRouter = require('./routes/declaraciones');
+//fin declaraciones
+
+
 var app = express();
 
 app.use(logger('dev'));
@@ -16,5 +31,19 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/api/s1', s1Router);
+
+// inicio declaraciones
+app.use('/users', usersRouter);
+app.use('/ciudades', ciudadesRouter);
+app.use('/entidades', entidadesRouter);
+app.use('/estadosciviles', estadoscivilesRouter);
+app.use('/regimenmatrimonial', regimenmatrimonialRouter);
+app.use('/municipios', municipiosRouter);
+app.use('/localidades', localidadesRouter);
+app.use('/tipovialidad', tipovialidadRouter);
+app.use('/declaraciones', declaracionesRouter);
+//fin declaraciones
+
+
 
 module.exports = app;
