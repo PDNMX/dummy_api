c820b6076eee4b175d7a6c7dc071e1c628a416fc
Benito Perdomo <benito.perdomo.pdn@gmail.com>
2018-11-12 10:33:36 -0600

    se corrigen las variables de entorno para compatibilidad con existentes

--- routes/database_config.js
+++ routes/database_config.js
@@ -1,11 +1,11 @@
 // Connection URL
 require('dotenv').config();
 
-const MONGO_USER = process.env.MONGO_USER;
-const MONGO_PASS = process.env.MONGO_PASS;
-const MONGO_HOST = process.env.MONGO_HOST;
-const MONGO_PORT = process.env.MONGO_PORT;
-const MONGO_DB = process.env.MONGO_DB;
+const MONGO_USER = process.env.DB_USER;
+const MONGO_PASS = process.env.DB_PASSWORD;
+const MONGO_HOST = process.env.DB_HOST;
+const MONGO_PORT = process.env.DB_PORT;
+const MONGO_DB = process.env.DB_NAME;
 
 const MONGO_DBAUTH = process.env.MONGO_DBAUTH;
 const MONGO_AUTHMECHANISM = process.env.MONGO_AUTHMECHANISM;
@@ -13,7 +13,8 @@ const MONGO_AUTHMECHANISM = process.env.MONGO_AUTHMECHANISM;
 let url = "mongodb://" + MONGO_HOST + ":" + MONGO_PORT;
 
 if (MONGO_USER !== "" && MONGO_PASS !== "") {
-  url = `mongodb://${MONGO_USER}:${MONGO_PASS}@${MONGO_HOST}:${MONGO_PORT}/?authMechanism=${MONGO_AUTHMECHANISM}&authSource=${MONGO_DBAUTH}`;
+  url = `mongodb://${MONGO_USER}:${MONGO_PASS}@${MONGO_HOST}:${MONGO_PORT}/${MONGO_DB}`;
+  // url = `mongodb://${MONGO_USER}:${MONGO_PASS}@${MONGO_HOST}:${MONGO_PORT}/?authMechanism=${MONGO_AUTHMECHANISM}&authSource=${MONGO_DBAUTH}`;
   console.log(url);
 }
 
