f246f767f0e9db11c29487ef9f2e1e5275a34b3c
Benito Perdomo <benito.perdomo.pdn@gmail.com>
2018-12-19 14:02:07 -0600

    catalgos avance

---
+++ routes/catBienesIntangibles.js
@@ -0,0 +1,50 @@
+var express = require("express");
+var router = express.Router();
+
+var dbmongo = require("./database_config");
+
+const MongoClient = require("mongodb").MongoClient;
+
+/* GET users listing. */
+router.get("/", function(req, res, next) {
+  let { codigo, valor } = req.query;
+
+  let query = {};
+
+  if (typeof valor !== "undefined") {
+    query.valor = {
+      $regex: valor,
+      $options: "i"
+    };
+  }
+
+  if (typeof codigo !== "undefined") {
+    query.codigo = codigo;
+  }
+
+  console.log(req.query);
+  console.log(query);
+
+  MongoClient.connect(
+    dbmongo.url,
+    {
+      useNewUrlParser: true
+    }
+  )
+    .then(client => {
+      let db = client.db(dbmongo.dbname);
+      let collection = db.collection("catBienesIntangibles");
+
+      collection
+        .find(query, { projection: { _id: 0 } })
+        .toArray(function(err, result) {
+          if (err) throw err;
+          res.json(result);
+        });
+    })
+    .catch(error => {
+      console.log(error);
+    });
+});
+
+module.exports = router;
---
+++ routes/documentosObtenidos.js
@@ -0,0 +1,38 @@
+var express = require("express");
+var router = express.Router();
+
+var dbmongo = require("./database_config");
+
+const MongoClient = require("mongodb").MongoClient;
+
+/* GET users listing. */
+router.get("/", function(req, res, next) {
+  MongoClient.connect(
+    dbmongo.url,
+    {
+      useNewUrlParser: true
+    }
+  )
+    .then(client => {
+      let db = client.db(dbmongo.dbname);
+      let collection = db.collection("documentoObtenido");
+
+      collection
+        .find(
+          {},
+          {
+            _id: false
+          }
+        )
+        .toArray(function(err, result) {
+          if (err) throw err;
+          //console.log(result);
+          res.json(result);
+        });
+    })
+    .catch(error => {
+      console.log(error);
+    });
+});
+
+module.exports = router;
