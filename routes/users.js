e868de7c66f3dcf89507296f5ecc72513e150b3c
Benito Perdomo <benito.perdomo.pdn@gmail.com>
2019-03-14 13:56:29 -0600

    se remueve el objectId de la consulta

--- routes/users.js
+++ routes/users.js
@@ -1,18 +1,18 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 var dbmongo = require("./database_config");
 
-const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
-const ObjectId = require('mongodb').ObjectId;
+const MongoClient = require("mongodb").MongoClient;
+const assert = require("assert");
+const ObjectId = require("mongodb").ObjectId;
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function(req, res, next) {
+  res.send("respond with a resource");
 });
 
-router.post('/login', function(req, res, next) {
+router.post("/login", function(req, res, next) {
   console.log(req.body);
 
   var result = {
@@ -21,54 +21,55 @@ router.post('/login', function(req, res, next) {
   };
   //console.log(result);
 
-  MongoClient.connect(dbmongo.url, {
-    useNewUrlParser: true
-  }).then(client => {
+  MongoClient.connect(
+    dbmongo.url,
+    {
+      useNewUrlParser: true
+    }
+  )
+    .then(client => {
+      let db = client.db(dbmongo.dbname);
+      let collection = db.collection("usuarios");
 
-    let db = client.db(dbmongo.dbname);
-    let collection = db.collection('usuarios');
+      console.log(req.query);
 
-    console.log(req.query);
+      const { usuario, clave } = req.body;
 
-    const {
-      usuario,
-      clave
-    } = req.body;
+      collection
+        .findOne(
+          {
+            usuario: usuario
+          },
+          { projection: { _id: 0 } }
+        )
+        .then(data => {
+          console.log(data);
 
+          if (data == null) {
+            res.send({
+              code: 205
+            });
+          } else {
+            if (data.clave == clave) {
+              res.send({
+                code: 200
+              });
+            } else {
+              res.send({
+                code: 204
+              });
+            }
+          }
 
-    collection.findOne({
-      "usuario": usuario
-    }).then(data => {
-      console.log(data);
-
-      if (data == null) {
-        res.send({
-          code:205
-        })
-      } else {
-        if (data.clave == clave) {
-          res.send({
-            code: 200
-          })
-        } else {
-          res.send({
-            code: 204
-          })
-        }
-      }
-
-      // res.json(data);
+          // res.json(data);
+        });
+    })
+    .catch(error => {
+      console.log(error);
     });
-
-  }).catch(error => {
-    console.log(error);
-  });
 
   // res.status(200);
   // res.send(result);
-
-
-
 });
 
 module.exports = router;
